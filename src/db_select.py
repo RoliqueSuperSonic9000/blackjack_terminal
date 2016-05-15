2531cc9006c1e352230a27f6fb771491a83b3938
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2016-05-14 21:45:28 -0400

    fixed untracked files

--- src/db_select.py
+++
@@ -1,9 +0,0 @@
-
-#!/usr/bin/env python
-import sqlite3
-conn = sqlite3.connect('test_db')
-c = conn.cursor()
-c.execute("""SELECT * FROM ROUNDS""")
-joe = c.fetchall()
-for row in joe:
-    print row
